7b4a0f06eaa0c6921cc2682029764521fda759a6
Roman Ness <kaossilat0r.rn@gmail.com>
2019-05-22 23:23:47 +0200

    add solution for 'triangle'

---
+++ triangle/triangle.go
@@ -0,0 +1,45 @@
+// Package triangle provides triangle utility methods
+package triangle
+
+import (
+	"errors"
+	"math"
+	"sort"
+)
+
+type Kind int
+
+const (
+    NaT = iota// not a triangle
+    Equ // equilateral
+    Iso // isosceles
+    Sca // scalene
+)
+
+// KindFromSides determines the kind of the triangle from the length of its sides
+func KindFromSides(a, b, c float64) Kind {
+
+	var err error
+	a, b, c, err = validateAndSortTriangleSides(a, b, c)
+	if err != nil || a + b < c {
+		return NaT
+	}
+	if a == b && b == c {
+		return Equ
+	}
+	if a == b || b == c || a == c {
+		return Iso
+	}
+	return Sca
+}
+
+func validateAndSortTriangleSides(a float64, b float64, c float64) (float64, float64, float64, error) {
+	sides := []float64{a, b, c}
+	for _, side := range sides {
+		if side <= 0 || math.IsNaN(side) || math.IsInf(side, 0) {
+			return 0, 0, 0, errors.New("at least one triangle side is invalid")
+		}
+	}
+	sort.Float64s(sides)
+	return sides[0], sides[1], sides[2], nil
+}
