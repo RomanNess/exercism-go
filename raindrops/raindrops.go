4e77d53b144eaf45951e58ed15976f13e665564a
Roman Ness <kaossilat0r.rn@gmail.com>
2019-05-31 16:48:17 +0200

    update solution for 'raindrops' to be less wasteful

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -4,32 +4,25 @@ import "strconv"
 
 // Convert provides a raindrop sound based on the factors of a given number
 func Convert(n int) (result string) {
-
-	for i := 1; i <= n; i++ {
-		if isFactorOf(i, n) {
-			result += numberToRaindrop(i)
+	for _, e := range factorsToSound {
+		if (n % e.factor) == 0 {
+			result += e.sound
 		}
 	}
 
-	if len(result) > 0 {
-		return result
+	if len(result) == 0 {
+		return strconv.Itoa(n)	// default
 	}
-	return strconv.Itoa(n)
+	return result
 }
 
-func isFactorOf(i int, n int) bool {
-	return (n % i) == 0
+type fToS struct {
+	factor int
+	sound string
 }
 
-func numberToRaindrop(i int) string {
-	switch i {
-	case 3:
-		return "Pling"
-	case 5:
-		return "Plang"
-	case 7:
-		return "Plong"
-	default:
-		return ""
-	}
+var factorsToSound = [...]fToS{
+	{3, "Pling"},
+	{5, "Plang"},
+	{7, "Plong"},
 }
