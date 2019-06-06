fc253c36758e3d4bf800a047f0f1f92364675a92
Roman Ness <roman.ness@cosee.biz>
2019-06-06 19:43:54 +0200

    add solution for 'listops'

--- list-ops/listops.go
+++ list-ops/listops.go
@@ -1,40 +1,84 @@
 package listops
 
+// IntList wraps a slice of integers
 type IntList []int
 
 type predFunc func(int) bool
 type unaryFunc func(int) int
 type binFunc func(int, int) int
 
-
-func (IntList) Length() int {
-	return -1
+// Length returns the number of elements in an IntList
+func (list IntList) Length() (len int) {
+	// without using len(list)
+	for range list {
+		len++
+	}
+	return len
 }
 
-func (IntList) Reverse() IntList {
-	return nil
+// Reverse provides the reverse of an IntList
+func (list IntList) Reverse() IntList {
+	for i, j := 0, list.Length()-1; i < j; i, j = i+1, j-1 {
+		list[i], list[j] = list[j], list[i]
+	}
+	return list
 }
 
-func (IntList) Append(list IntList) IntList {
-	return nil
+// Append appends a given list to an IntList
+func (list IntList) Append(other IntList) IntList {
+
+	lLength := list.Length()
+	otherLength := other.Length()
+
+	result := make(IntList, lLength+otherLength)
+	copy(result, list)
+
+	for i := 0; i < otherLength; i++ {
+		result[lLength+i] = other[i]
+	}
+	return result
 }
 
-func (IntList) Concat(lists []IntList) IntList {
-	return nil
+// Concat appends a given list of lists to an IntList
+func (list IntList) Concat(otherLists []IntList) (result IntList) {
+	result = list
+	for _, otherList := range otherLists {
+		result = result.Append(otherList)
+	}
+	return result
 }
 
-func (IntList) Filter(condition predFunc) IntList {
-	return nil
+// Filter provides a list with only the elements that match a given predicate
+func (list IntList) Filter(pred predFunc) (result IntList) {
+	result = IntList{}
+	for _, e := range list {
+		if pred(e) {
+			result = append(result, e)
+		}
+	}
+	return result
 }
 
-func (IntList) Map(condition unaryFunc) IntList {
-	return nil
+// Map applies a given function to every element of an IntList
+func (list IntList) Map(fn unaryFunc) IntList {
+	for i, e := range list {
+		list[i] = fn(e)
+	}
+	return list
 }
 
-func (IntList) Foldr(condition binFunc, init int) int {
-	return -1
+// Foldr performs a right fold with a given function on an IntList
+func (list IntList) Foldr(fn binFunc, init int) int {
+	if list.Length() == 0 {
+		return init
+	}
+	return list[:list.Length()-1].Foldr(fn, fn(list[list.Length()-1], init))
 }
 
-func (IntList) Foldl(condition binFunc, init int) int {
-	return -1
+// Foldl performs a left fold with a given function on an IntList
+func (list IntList) Foldl(fn binFunc, init int) int {
+	if list.Length() == 0 {
+		return init
+	}
+	return list[1:].Foldl(fn, fn(init, list[0]))
 }
